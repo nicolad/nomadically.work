be67c4e0c3e7dc5b0af2c426f2352a9512fc2f1c
Vadim Nicolai <nicolai.vadim@gmail.com>
2026-02-06 11:50:04 +0200

    feat: constrain header within container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "@radix-ui/themes/styles.css";
 import "./globals.css";
-import { Theme, Flex } from "@radix-ui/themes";
+import { Theme, Flex, Container } from "@radix-ui/themes";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
 import Link from "next/link";
@@ -20,27 +20,29 @@ export default function RootLayout({
     <html lang="en" style={{ fontFamily: "var(--font-geist-sans)" }}>
       <body>
         <Theme appearance="dark">
-          <Flex asChild justify="between" align="center" p="4" px="6">
-            <header>
-              <Link href="/" style={{ display: "flex" }}>
-                <Image
-                  src="/logo.svg"
-                  alt="Nomadically Logo"
-                  width={600}
-                  height={100}
-                  priority
-                />
-              </Link>
-              <Link
-                href="https://github.com/nicolad/nomadically.work"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ display: "flex", alignItems: "center" }}
-              >
-                <GitHubLogoIcon width={32} height={32} style={{ color: "#888888" }} />
-              </Link>
-            </header>
-          </Flex>
+          <header>
+            <Container size="4">
+              <Flex justify="between" align="center" p="4">
+                <Link href="/" style={{ display: "flex" }}>
+                  <Image
+                    src="/logo.svg"
+                    alt="Nomadically Logo"
+                    width={600}
+                    height={100}
+                    priority
+                  />
+                </Link>
+                <Link
+                  href="https://github.com/nicolad/nomadically.work"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ display: "flex", alignItems: "center" }}
+                >
+                  <GitHubLogoIcon width={32} height={32} style={{ color: "#888888" }} />
+                </Link>
+              </Flex>
+            </Container>
+          </header>
           {children}
         </Theme>
       </body>

ad8d5378fcefede4a7e5fdbfb368831276ff4b2b
Vadim Nicolai <nicolai.vadim@gmail.com>
2026-02-11 17:21:17 +0200

    add button to applications page

--- src/app/applications/page.tsx
+++ src/app/applications/page.tsx
@@ -1,11 +1,15 @@
-import { Container, Heading, Text } from "@radix-ui/themes";
+import { Container, Heading, Text, Button, Flex } from "@radix-ui/themes";
+import { PlusIcon } from "@radix-ui/react-icons";
 
 export default function ApplicationsPage() {
   return (
     <Container size="4" p="8">
-      <Heading size="8" mb="4">
-        Applications
-      </Heading>
+      <Flex justify="between" align="center" mb="4">
+        <Heading size="8">Applications</Heading>
+        <Button>
+          <PlusIcon /> Add Application
+        </Button>
+      </Flex>
       <Text color="gray">Your job applications will appear here.</Text>
     </Container>
   );

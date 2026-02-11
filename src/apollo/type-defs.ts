6b2eb9219ef67585359e704e3e40c02e70456aec
Vadim Nicolai <nicolai.vadim@gmail.com>
2026-02-11 18:35:20 +0200

    clean up leftover code in type-defs.ts

--- src/apollo/type-defs.ts
+++ src/apollo/type-defs.ts
@@ -5,7 +5,9 @@ import { gql } from "@apollo/client";
 const schemaFiles = loadFilesSync(join(process.cwd(), "schema/**/*.graphql"));
 
 export const typeDefs = schemaFiles.map((schema) =>
-  typeof schema === "string" ? gql`${schema}` : schema
+  typeof schema === "string"
+    ? gql`
+        ${schema}
+      `
+    : schema,
 );
-  ${schemaString}
-`;

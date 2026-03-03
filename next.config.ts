bf608aac16da8329e0d4c2bfdcea192bd296034f
Vadim Nicolai <nicolai.vadim@gmail.com>
2026-03-03 17:35:57 +0200

    Small improvement.

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  experimental: {
-    instrumentationHook: true,
-  },
   async rewrites() {
     return [
       {
